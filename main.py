764b9e6a0a96cc616d9ef8ea8632d6cbf5806fdf
nouveau217 <bbssssiuy@gmail.com>
2026-09-04 18:08:08 +0700

    Update main.py

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ def chat():
                 "Content-Type": "application/json",
             },
             json={
-                "model": "z-ai/glm-5.2:free",
+                "model": "openai/gpt-oss-20b:free",
                 "messages": [
                     {"role": "user", "content": prompt}
                 ]
